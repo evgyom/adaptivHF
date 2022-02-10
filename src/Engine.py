97c42cb15ad74f06ef2b1f7240f1b50e38d819c1
nata108 <natabara@gyongyossy.hu>
2022-02-10 22:31:49 +0100

    Base tick functions

--- src/Engine.py
+++ src/Engine.py
@@ -13,8 +13,9 @@ class RandBotStrategy:
     def getNextAction(self):
         pass
 
+
 class Player:
-    strategies = {"randombot":RandBotStrategy,}
+    strategies = {"randombot": RandBotStrategy, }
 
     def __init__(self, name, playerType, startingSize):
         self.name = name
@@ -24,22 +25,50 @@ class Player:
         self.strategy = Player.strategies[playerType]
         self.active = True
 
+    def die(self):
+        pass
+
 
 class AdaptIOEngine:
+    neighbourDifferencies = {0: np.array([0, 1]), 1: np.array([0, -1]), 2: np.array([1, 0]), 3: np.array([-1, 0])}
+
+    @staticmethod
+    def getRandomNeighbor(pos):
+        r = random.randint(0, 3)
+        if r == 0:
+            return pos + np.array([0, 1])
+        elif r == 1:
+            return pos + np.array([0, -1])
+        elif r == 2:
+            return pos + np.array([1, 0])
+        elif r == 3:
+            return pos + np.array([-1, 0])
+
     def __init__(self, mapstring, startingSize, minRatio, strategyDict, visionRange, updateMode):
         self.mapstring = mapstring
-        self.field = self.prepareField()
+        self.field, self.size = self.prepareField()
         self.startField = self.field
         self.startingSize = startingSize
         self.minRatio = minRatio
         self.visionRange = visionRange
         self.updateMode = updateMode
-
+        self.visibilityMask = self.genVisibilityMask()
 
         ids = list(range(4))
         random.shuffle(ids)
 
-        self.players = [Player(list(strategyDict.keys())[ids[i]], list(strategyDict.values())[ids[i]], self.startingSize) for i in range(4)]
+        self.players = [
+            Player(list(strategyDict.keys())[ids[i]], list(strategyDict.values())[ids[i]], self.startingSize) for i in
+            range(4)]
+
+    def genVisibilityMask(self):
+        coordlist = []
+        for i in np.arange(-self.visionRange, self.visionRange+1):
+            for j in np.arange(-self.visionRange, self.visionRange+1):
+                if np.sqrt(i**2+j**2)<=self.visionRange:
+                    coordlist.append((i, j))
+
+        return coordlist
 
     def prepareField(self):
         # ertekek self.field-be, initben setelje valahogy, ugy hogy 0-4 kaja ertek, -1 fal
@@ -51,19 +80,72 @@ class AdaptIOEngine:
         pass
 
     def makeAction(self, action, pos):
-        pass
+        if action=="0":
+            return pos
+        if action[0]=="-":
+            pos[0]-=1
+        if action[0]=="+":
+            pos[0]+=1
+        if action[1]=="-":
+            pos[1]-=1
+        if action[1]=="+":
+            pos[1]+=1
+        return pos
 
     def handleCollision(self, positions, oldPositions, valueToHandle):
-        pass
+        newpos = positions.copy()
+        displacements = list(self.neighbourDifferencies.values())
+        random.shuffle(displacements)
+
+        colliding = []
+        for i in range(len(positions)):
+            if positions[i] == valueToHandle:
+                colliding.append(i)
+
+        sizes = np.array([self.players[i].size for i in colliding])
+        idx = np.argsort(sizes)
+        if sizes[idx[0]] >= sizes[idx[1]]*self.minRatio:
+            newsize = np.sum(sizes)
+            colliding.remove(colliding[idx[0]])
+            for dead in colliding:
+                self.players[dead].die()
+
+            self.players[idx[0]].size = newsize
+
+        else:
+            j = 0
+            for i in colliding:
+                newpos[i] += displacements[j]
+                j += 1
+        return newpos
 
     def surveyArea(self, pos):
-        pass
+        observation = {"pos": pos, "vision":[]}
+        playerpos = [player.pos for player in self.players]
+        for diffcoord in self.visibilityMask:
+            vispos = pos + np.array(diffcoord)
+            if vispos in playerpos:
+                playerDict = {}
+                playerDict["size"] = self.players[playerpos.index(vispos)].size
+                playerDict["name"] = self.players[playerpos.index(vispos)].name
+            else:
+                playerDict = None
+
+            if not 0<=vispos[0]<=self.size or not 0<=vispos[1]<=self.size:
+                val = -1
+            else:
+                val = self.field[vispos[0],vispos[1]]
+            observation["vision"].append({"relative_coord":diffcoord, "value":val, "player":playerDict})
+
+        return observation
+
 
     def updatePlayers(self, newpos):
-        pass
+        for i in range(len(self.players)):
+            self.players[i].pos = newpos[i]
 
     def updateFood(self):
-        if self.updateMode=="static":
+        if self.updateMode == "static":
             return
         else:
             pass
@@ -80,7 +162,6 @@ class AdaptIOEngine:
 
         return newpos
 
-
     def tick(self):
         actions = [player.strategy.getNextAction() for player in self.players]
         pos = [player.pos for player in self.players]
@@ -94,4 +175,3 @@ class AdaptIOEngine:
 
         for i in range(len(self.players)):
             self.players[i].strategy.setObservations(self.players[i].pos, self.surveyArea(self.players[i].pos))
-
